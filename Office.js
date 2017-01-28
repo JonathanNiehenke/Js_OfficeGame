5d2fd9bb65e7e6c5970c8cf342d2031ac63ea54d
JonathanNiehenke <jrniehenke@gmail.com>
2017-01-28 11:35:44 -0800

    Move Engine as library and display inventory

--- Office.js
+++ Office.js
@@ -1,101 +1,8 @@
 let db = console.log;
 
-function Engine(Tile, Inventory) {
-    this.Tile = Tile;
-    this.Inventory = Inventory;
-    this.Levels = undefined;  // Will hold function generator of levels;
-    this.Environment = {};
-    this.keyInput = {
-        // 27: "escape",
-        37: [0, -1], // Left Arrow
-        38: [-1, 0], // Up Arrow
-        39: [0, 1],  // Right Arrow
-        40: [1, 0],  // Down Arrow
-        74: [0, -1], // Left Arrow
-        73: [-1, 0], // Up Arrow
-        76: [0, 1],  // Right Arrow
-        75: [1, 0],  // Down Arrow
-        65: [0, -1], // Left Arrow
-        87: [-1, 0], // Up Arrow
-        68: [0, 1],  // Right Arrow
-        83: [1, 0],  // Down Arrow
-        handle: function(keyEvent) {
-            let Movement = this.keyInput[keyEvent.keyCode];
-            if (Movement) {
-                let moveTo = [this.Environment.player[0] + Movement[0],
-                              this.Environment.player[1] + Movement[1]];
-                let cellTo = this.Environment.cell[moveTo];
-                let cellAction = this.Tile[cellTo].action;
-                if (moveTo in this.Environment.cell && cellAction) {
-                    cellAction.call(this, moveTo, cellTo);
-                }
-            }
-        },
-    };
-    this.establishEnvironment = function(Environment, Level) {
-        let structureEl = document.getElementById("Structure");
-        structureEl.innerHTML = "";  // Removing all decendants.
-        let [Messages, Structure] = Level;
-        let colLength = Structure.length;
-        for (let x = 0; x < colLength; ++x) {
-            let rowDiv = document.createElement("div");
-            let Row = Structure[x];
-            let rowLength = Row.length;
-            for (let y = 0; y < rowLength; ++y) {
-                let Cell = {index: [x, y], value: Row[y]};
-                Environment.cell[Cell.index] = Cell.value;
-                pushKey(Environment.cellLocations, Cell.value, Cell.index);
-                let newTile = this.Tile[Cell.value].image.cloneNode();
-                rowDiv.appendChild(newTile);
-            }
-            rowDiv.id = `row${x}`;
-            structureEl.appendChild(rowDiv);
-        }
-    };
-    this.nextEnvironment = function() {
-        let Level = this.Levels.next().value;
-        let Environment = {
-                cell: {},
-                cellLocations: {},
-                onCell: ' ',
-                player: [0, 0],
-                end: [0, 0],
-                requirements: 0
-        };
-        if (Level) {
-            this.establishEnvironment(Environment, Level);
-            startIndex = Environment.cellLocations['$'][0];
-            Environment.cell[startIndex] = ' ';
-            Environment.player = startIndex;
-            let requirements = Environment.requirements;
-            requirements += (Environment.cellLocations['e'] || []).length;
-            requirements += (Environment.cellLocations['p'] || []).length;
-            requirements += (Environment.cellLocations['P'] || []).length;
-            endLocations = Environment.cellLocations['E'];
-            endIndex = endLocations ? endLocations[0] : startIndex;
-            Environment.end = endIndex;
-        }
-        return Environment;
-    };
-    this.replaceImage = function(Index, cellValue) {
-        let rowDiv = document.getElementById(`row${Index[0]}`);
-        let newImgEl = this.Tile[cellValue].image;
-        let currentImgEl = rowDiv.getElementsByTagName("img")[Index[1]];
-        rowDiv.replaceChild(newImgEl.cloneNode(), currentImgEl);
-    };
-    this.replaceCell = function(Index, cellValue) {
-        this.replaceImage(Index, cellValue);
-    };
-    this.movePlayer = function(moveTo) {
-        let Environment = this.Environment;
-        this.replaceImage(Environment.player, Environment.onCell);
-        this.replaceImage(moveTo, "$");
-        Environment.onCell = Environment.cell[moveTo];
-        Environment.player = moveTo;
-    };
-}
-
 function OfficeGame() {
+    // Tile actions requires the Engine prototype. By manually
+    // assigning them later we avoid the chicken and the egg paradox.
     this.__proto__ = new Engine();
     this.parseLevelFile = function*(levelFile) {
         let fileLines = levelFile.target.result.split("\r\n");
@@ -125,28 +32,39 @@ function OfficeGame() {
             this.Environment = this.nextEnvironment();
         }
         let levelFile = fileEvent.target.files[0];
-        let Reader = new FileReader();
         if (levelFile) {
+            let Reader = new FileReader();
             Reader.onload = onFileLoad.bind(this);
-            Reader.readAsText(levelFile);
+            Reader.readAsText(levelFile);  // Calls Reader.onload.
+            document.getElementById("Game").className = ""
+            document.getElementById("officeLevels").className = "Hidden"
         }
     },
     this.openCell = function(moveTo) {
         this.Environment.cell[moveTo] = " ";
         this.movePlayer(moveTo);
     }
+    this.replaceKeyImage = function(keyValue, cellValue) {
+        let keyId = `Key_${keyValue}`;
+        let newImgEl = this.Tile[cellValue].image.cloneNode();
+        let currentImgEl = document.getElementById(keyId);
+        currentImgEl.parentNode.replaceChild(newImgEl, currentImgEl);
+        newImgEl.id = keyId;  // Reset the id for later use.
+    };
     this.pickupKey = function(moveTo) {
-        let cellTo = this.Environment.cell[moveTo];
-        if (!this.Inventory.key[cellTo]) {
-            this.Inventory.key[cellTo] = 1;
+        let cellToValue = this.Environment.cell[moveTo];
+        if (!this.Inventory.key[cellToValue]) {
+            this.Inventory.key[cellToValue] = 1;
             this.openCell(moveTo);
+            this.replaceKeyImage(cellToValue, cellToValue);
         }
     },
-    this.openKeyLock = function(moveTo, cellTo) {
-        let requiredKey = cellTo.toLowerCase();
+    this.openKeyLock = function(moveTo, cellToValue) {
+        let requiredKey = cellToValue.toLowerCase();
         if (this.Inventory.key[requiredKey]) {
             this.Inventory.key[requiredKey] = 0;
             this.openCell(moveTo);
+            this.replaceKeyImage(requiredKey, " ");
         }
     },
     this.changeRequirements = function(value) {
@@ -216,8 +134,7 @@ function OfficeGame() {
             "r": {image: getImg("Computer"),
                   action: function(moveTo) {
                         for (Index of this.Environment.cellLocations["p"]) {
-                            this.Environment.cell[Index] = "P";
-                            this.replaceImage(Index, "P");
+                            this.replaceCell(Index, "P");
                         }
                   }
             },
@@ -227,8 +144,7 @@ function OfficeGame() {
             "p": {image: getImg("PrinterX"), action: undefined},
             "P": {image: getImg("Printer"),
                   action: function(moveTo) {
-                    this.Environment.cell[moveTo] = "#";
-                    this.replaceImage(moveTo, "#");
+                    this.replaceCell(moveTo, "#");
                     this.changeRequirements(-1);
                   }
             },
