04e29d87d02ee5cf3679ea2ea54ca67be710df7e
JonathanNiehenke <jrniehenke@gmail.com>
2017-01-28 22:26:51 -0800

    Introduce object tutorial level mechanics

--- Office.js
+++ Office.js
@@ -15,17 +15,17 @@ function OfficeGame() {
                 Messages = [];
                 Structure = [];
             }
-            else if (Begin == '"') {
+            else if (Begin === "\"") {
                 Messages.push(Line);
             }
-            else if (Begin == "\\" || !Begin) {
+            else if (Begin === "\\" || !Begin) {
                 continue;
             }
             else {
                 Structure.push(Line);
             }
         }
-    },
+    };
     this.handleFileEvent = function(fileEvent) {
         function onFileLoad(levelFile) {
             this.Levels = this.parseLevelFile(levelFile);
@@ -36,14 +36,14 @@ function OfficeGame() {
             let Reader = new FileReader();
             Reader.onload = onFileLoad.bind(this);
             Reader.readAsText(levelFile);  // Calls Reader.onload.
-            document.getElementById("Game").className = ""
-            document.getElementById("officeLevels").className = "Hidden"
+            document.getElementById("Game").className = "";
+            document.getElementById("officeLevels").className = "Hidden";
         }
-    },
+    };
     this.openCell = function(moveTo) {
         this.Environment.cell[moveTo] = " ";
         this.movePlayer(moveTo);
-    }
+    };
     this.replaceKeyImage = function(keyValue, cellValue) {
         let keyId = `Key_${keyValue}`;
         let newImgEl = this.Tile[cellValue].image.cloneNode();
@@ -52,127 +52,157 @@ function OfficeGame() {
         newImgEl.id = keyId;  // Reset the id for later use.
     };
     this.pickupKey = function(moveTo) {
-        let cellToValue = this.Environment.cell[moveTo];
-        if (!this.Inventory.key[cellToValue]) {
-            this.Inventory.key[cellToValue] = 1;
+        let cellTo = this.Environment.cell[moveTo];
+        if (!this.Inventory.key[cellTo]) {
+            this.Inventory.key[cellTo] = 1;
             this.openCell(moveTo);
-            this.replaceKeyImage(cellToValue, cellToValue);
+            this.replaceKeyImage(cellTo, cellTo);
         }
-    },
-    this.openKeyLock = function(moveTo, cellToValue) {
-        let requiredKey = cellToValue.toLowerCase();
+    };
+    this.openKeyLock = function(moveTo, cellTo) {
+        let requiredKey = cellTo.toLowerCase();
         if (this.Inventory.key[requiredKey]) {
             this.Inventory.key[requiredKey] = 0;
             this.openCell(moveTo);
             this.replaceKeyImage(requiredKey, " ");
         }
-    },
+    };
     this.changeRequirements = function(value) {
-        this.requirements += value;
-        let changeTo = this.requirements ? " " : "E";
+        this.Environment.requirements += value;
+        let changeTo = this.Environment.requirements ? " " : "E";
         let endIndex = this.Environment.end;
         this.Environment.cell[endIndex] = changeTo;
         this.replaceImage(endIndex, changeTo);
-    },
-    this.constructInventory = function() {
-        Inventory = {
-            key: {a: 0, b: 0, c: 0, d: 0},
-            map: {},
-            object: '@',
+        db("req change:", this.Environment.requirements);
+    };
+    this.__replaceObject = function(cellValue) {
+        this.Inventory.object = cellValue;
+        let newImgEl = this.Tile[cellValue].image.cloneNode();
+        let currentImgEl = document.getElementById("Object");
+        currentImgEl.parentNode.replaceChild(newImgEl, currentImgEl);
+        newImgEl.id = "Object";  // Reset the id for later use.
+        this.changeRequirements(cellValue === "@" ? -1 : 1);
+        // self.environment.replace_tiles(':', self.tiles[':'])
+    };
+    this.pickupObject = function(moveTo, cellTo) {
+        if (this.Inventory.object === "@") {
+            this.__replaceObject(cellTo);
+            this.openCell(moveTo);
         }
+    };
+    this.dropObject = function(moveTo, cellTo) {
+        let object = cellTo.toLowerCase();
+        if (object === this.Inventory.object) {
+            this.__replaceObject("@");
+        }
+    }
+    this.__replaceMap = function(moveTo, cellTo) {
+        this.Inventory.map[moveTo] = cellTo;
+        this.replaceImage(moveTo, cellTo);
+    };
+    this.swapObjects = function(moveTo, cellTo) {
+        let fromHand = this.Inventory.object;
+        let fromDrop = this.Inventory.map[moveTo] || "@";
+        this.__replaceObject(fromDrop);
+        this.__replaceMap(moveTo, fromHand);
+        if (fromDrop !== "@" && fromHand !== "@" ) {  // Swap Item
+            // Refund extra requirement cause by swap.
+            this.changeRequirements(-1);
+        }
+    };
+    this.__constructInventory = function() {
+        Inventory = {
+            "key": {"a": 0, "b": 0, "c": 0, "d": 0},
+            "map": {},
+            "object": "@",
+        };
         return Inventory;
-    },
-    this.constructTiles = function() {
+    };
+    this.__constructTiles = function() {
         let getImg = document.getElementById.bind(document);
         Tile = {
-            "$": {image: getImg("Player"), action: undefined},
-            " ": {image: getImg("Empty"), action: this.movePlayer},
-            "#": {image: getImg("Wall"), action: undefined},
-            ":": {image: getImg("OpenNarrow"), action: undefined},
-            ";": {image: getImg("ClosedNarrow"), action: undefined},
-            "@": {image: getImg("DropZone"), action: undefined},
-            "a": {image: getImg("RedKey"), action: this.pickupKey},
-            "b": {image: getImg("BlueKey"), action: this.pickupKey},
-            "c": {image: getImg("GreenKey"), action: this.pickupKey},
-            "d": {image: getImg("YellowKey"), action: this.pickupKey},
-            "A": {image: getImg("RedLock"), action: this.openKeyLock},
-            "B": {image: getImg("BlueLock"), action: this.openKeyLock},
-            "C": {image: getImg("GreenLock"), action: this.openKeyLock},
-            "D": {image: getImg("YellowLock"), action: this.openKeyLock},
-            "e": {image: getImg("Source"),
-                  action: function(moveTo) {
+            "$": {"image": getImg("Player"), "action": undefined},
+            " ": {"image": getImg("Empty"), "action": this.movePlayer},
+            "#": {"image": getImg("Wall"), "action": undefined},
+            ":": {"image": getImg("OpenNarrow"), "action": undefined},
+            ";": {"image": getImg("ClosedNarrow"), "action": undefined},
+            "@": {"image": getImg("DropZone"), "action": this.swapObjects},
+            "a": {"image": getImg("RedKey"), "action": this.pickupKey},
+            "b": {"image": getImg("BlueKey"), "action": this.pickupKey},
+            "c": {"image": getImg("GreenKey"), "action": this.pickupKey},
+            "d": {"image": getImg("YellowKey"), "action": this.pickupKey},
+            "A": {"image": getImg("RedLock"), "action": this.openKeyLock},
+            "B": {"image": getImg("BlueLock"), "action": this.openKeyLock},
+            "C": {"image": getImg("GreenLock"), "action": this.openKeyLock},
+            "D": {"image": getImg("YellowLock"), "action": this.openKeyLock},
+            "e": {
+                "image": getImg("Source"),
+                "action": function(moveTo) {
                     this.Environment.cell[moveTo] = " ";
                     this.movePlayer(moveTo);
                     this.changeRequirements(-1);
-                  }
+                }
             },
-            "E": {image: getImg("Elevator"),
-                  action: function(moveTo) {
-                        this.Environment = this.nextEnvironment();
-                  }
+            "E": {
+                "image": getImg("Elevator"),
+                "action": function(moveTo) {
+                    this.Environment = this.nextEnvironment();
+                }
             },
-            "f": {image: getImg("LockNumber"), action: undefined},
-            "F": {image: getImg("PinLock"), action: undefined},
-            "g": {image: getImg("Cart"), action: undefined},
-            "G": {image: getImg("Plant"), action: undefined},
-            "h": {image: getImg("Papers"), action: undefined},
-            "H": {image: getImg("Desk"), action: undefined},
-            "i": {image: getImg("Trash"), action: undefined},
-            "I": {image: getImg("TrashCan"), action: undefined},
-            "j": {image: getImg("Mop"), action: undefined},
-            "J": {image: getImg("WetFloor"), action: undefined},
-            "k": {image: getImg("Flashlight"), action: undefined},
-            "K": {image: getImg("Darkness"), action: undefined},
-            "l": {image: getImg("LightOff"), action: undefined},
-            "L": {image: getImg("LightOn"), action: undefined},
-            "m": {image: getImg("MotionOn"), action: undefined},
-            "M": {image: getImg("MotionOff"), action: undefined},
-            "n": {image: getImg("MotionNumber"), action: undefined},
-            "N": {image: getImg("Signal"), action: undefined},
-            "q": {image: getImg("LightPlug"), action: undefined},
-            "Q": {image: getImg("Empty"), action: undefined},
-            "r": {image: getImg("Computer"),
-                  action: function(moveTo) {
-                        for (Index of this.Environment.cellLocations["p"]) {
-                            this.replaceCell(Index, "P");
-                        }
-                  }
+            "f": {"image": getImg("LockNumber"), "action": undefined},
+            "F": {"image": getImg("PinLock"), "action": undefined},
+            "g": {"image": getImg("Cart"), "action": this.pickupObject},
+            "G": {"image": getImg("Plant"), "action": this.pickupObject},
+            "h": {"image": getImg("Papers"), "action": this.pickupObject},
+            "H": {"image": getImg("Desk"), "action": this.dropObject},
+            "i": {"image": getImg("Trash"), "action": this.pickupObject},
+            "I": {"image": getImg("TrashCan"), "action": this.dropObject},
+            "j": {"image": getImg("Mop"), "action": undefined},
+            "J": {"image": getImg("WetFloor"), "action": undefined},
+            "k": {"image": getImg("Flashlight"), "action": undefined},
+            "K": {"image": getImg("Darkness"), "action": undefined},
+            "l": {"image": getImg("LightOff"), "action": undefined},
+            "L": {"image": getImg("LightOn"), "action": undefined},
+            "m": {"image": getImg("MotionOn"), "action": undefined},
+            "M": {"image": getImg("MotionOff"), "action": undefined},
+            "n": {"image": getImg("MotionNumber"), "action": undefined},
+            "N": {"image": getImg("Signal"), "action": undefined},
+            "q": {"image": getImg("LightPlug"), "action": undefined},
+            "Q": {"image": getImg("Empty"), "action": undefined},
+            "r": {
+                "image": getImg("Computer"),
+                "action": function(moveTo) {
+                    for (Index of this.Environment.cellLocations["p"]) {
+                        this.replaceCell(Index, "P");
+                    }
+                }
             },
-            "R": {image: getImg("ComputerPlug"), action: undefined},
-            "s": {image: getImg("Socket"), action: undefined},
-            "S": {image: getImg("PluggedSocket"), action: undefined},
-            "p": {image: getImg("PrinterX"), action: undefined},
-            "P": {image: getImg("Printer"),
-                  action: function(moveTo) {
+            "R": {"image": getImg("ComputerPlug"), "action": undefined},
+            "s": {"image": getImg("Socket"), "action": undefined},
+            "S": {"image": getImg("PluggedSocket"), "action": undefined},
+            "p": {"image": getImg("PrinterX"), "action": undefined},
+            "P": {
+                "image": getImg("Printer"),
+                "action": function(moveTo) {
                     this.replaceCell(moveTo, "#");
                     this.changeRequirements(-1);
-                  }
+                }
             },
-            "0": {image: getImg("DD0"), action: undefined},
-            "1": {image: getImg("DD1"), action: undefined},
-            "2": {image: getImg("DD2"), action: undefined},
-            "3": {image: getImg("DD3"), action: undefined},
-            "4": {image: getImg("DD4"), action: undefined},
-            "5": {image: getImg("DD5"), action: undefined},
-            "6": {image: getImg("DD6"), action: undefined},
-            "7": {image: getImg("DD7"), action: undefined},
-            "8": {image: getImg("DD8"), action: undefined},
-            "9": {image: getImg("DD9"), action: undefined},
+            "0": {"image": getImg("DD0"), "action": undefined},
+            "1": {"image": getImg("DD1"), "action": undefined},
+            "2": {"image": getImg("DD2"), "action": undefined},
+            "3": {"image": getImg("DD3"), "action": undefined},
+            "4": {"image": getImg("DD4"), "action": undefined},
+            "5": {"image": getImg("DD5"), "action": undefined},
+            "6": {"image": getImg("DD6"), "action": undefined},
+            "7": {"image": getImg("DD7"), "action": undefined},
+            "8": {"image": getImg("DD8"), "action": undefined},
+            "9": {"image": getImg("DD9"), "action": undefined},
         };
         return Tile;
     };
-    this.Tile = this.constructTiles();
-    this.Inventory = this.constructInventory();
-}
-
-function pushKey(obj, key, value) {
-    let arr = obj[key];
-    if (arr) {
-        arr.push(value);
-    }
-    else {
-        obj[key] = [value];
-    }
+    this.Tile = this.__constructTiles();
+    this.Inventory = this.__constructInventory();
 }
 
 function init() {
--- TileEngine.js
+++ TileEngine.js
@@ -1,3 +1,13 @@
+function pushKey(obj, key, value) {
+    let arr = obj[key];
+    if (arr) {
+        arr.push(value);
+    }
+    else {
+        obj[key] = [value];
+    }
+}
+
 function Engine(Tile, Inventory) {
     this.Tile = Tile;
     this.Inventory = Inventory;
@@ -5,22 +15,23 @@ function Engine(Tile, Inventory) {
     this.Environment = {};
     this.keyInput = {
         // 27: "escape",
-        38: [-1, 0], // Up Arrow
-        40: [1, 0],  // Down Arrow
-        37: [0, -1], // Left Arrow
-        39: [0, 1],  // Right Arrow
-        87: [-1, 0], // W Key (Up)
-        83: [1, 0],  // S Key (Down)
-        65: [0, -1], // A Key (Left)
-        68: [0, 1],  // D Key (Right)
-        73: [-1, 0], // I key (Up)
-        75: [1, 0],  // K Key (Down)
-        74: [0, -1], // J key (Left)
-        76: [0, 1],  // L key (Right)
-        handle: function(keyEvent) {
+        "38": [-1, 0], // Up Arrow
+        "40": [1, 0],  // Down Arrow
+        "37": [0, -1], // Left Arrow
+        "39": [0, 1],  // Right Arrow
+        "87": [-1, 0], // W Key (Up)
+        "83": [1, 0],  // S Key (Down)
+        "65": [0, -1], // A Key (Left)
+        "68": [0, 1],  // D Key (Right)
+        "73": [-1, 0], // I key (Up)
+        "75": [1, 0],  // K Key (Down)
+        "74": [0, -1], // J key (Left)
+        "76": [0, 1],  // L key (Right)
+        "handle": function(keyEvent) {
             let Movement = this.keyInput[keyEvent.keyCode]; if (Movement) {
-                let moveTo = [this.Environment.player[0] + Movement[0],
-                              this.Environment.player[1] + Movement[1]];
+                let moveTo = [
+                    this.Environment.player[0] + Movement[0],
+                    this.Environment.player[1] + Movement[1]];
                 let cellTo = this.Environment.cell[moveTo];
                 let cellAction = this.Tile[cellTo].action;
                 if (moveTo in this.Environment.cell && cellAction) {
@@ -41,7 +52,7 @@ function Engine(Tile, Inventory) {
             let Row = Structure[x];
             let rowLength = Row.length;
             for (let y = 0; y < rowLength; ++y) {
-                let Cell = {index: [x, y], value: Row[y]};
+                let Cell = {"index": [x, y], "value": Row[y]};
                 Environment.cell[Cell.index] = Cell.value;
                 pushKey(Environment.cellLocations, Cell.value, Cell.index);
                 let newTile = this.Tile[Cell.value].image.cloneNode();
@@ -54,23 +65,24 @@ function Engine(Tile, Inventory) {
     this.nextEnvironment = function() {
         let Level = this.Levels.next().value;
         let Environment = {
-                cell: {},
-                cellLocations: {},
-                onCell: ' ',
-                player: [0, 0],
-                end: [0, 0],
-                requirements: 0
+            "cell": {},
+            "cellLocations": {},
+            "onCell": " ",
+            "player": [0, 0],
+            "end": [0, 0],
+            "requirements": 0,
         };
         if (Level) {
             this.establishEnvironment(Environment, Level);
-            startIndex = Environment.cellLocations['$'][0];
-            Environment.cell[startIndex] = ' ';
+            startIndex = Environment.cellLocations["$"][0];
+            Environment.cell[startIndex] = " ";
             Environment.player = startIndex;
-            let requirements = Environment.requirements;
-            requirements += (Environment.cellLocations['e'] || []).length;
-            requirements += (Environment.cellLocations['p'] || []).length;
-            requirements += (Environment.cellLocations['P'] || []).length;
-            endLocations = Environment.cellLocations['E'];
+            Environment.requirements = (
+                (Environment.cellLocations["e"] || []).length +
+                (Environment.cellLocations["p"] || []).length +
+                (Environment.cellLocations["P"] || []).length)
+            db("req set:", Environment.requirements);
+            endLocations = Environment.cellLocations["E"];
             endIndex = endLocations ? endLocations[0] : startIndex;
             Environment.end = endIndex;
         }
@@ -94,4 +106,3 @@ function Engine(Tile, Inventory) {
         Environment.player = moveTo;
     };
 }
-
